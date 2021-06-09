62edba95a6e750b2396a83c0a733fdcd2cbad928
Martin <byrlukmartin@gmail.com>
2021-06-09 23:02:34 +0300

    before build

--- src/config/api.js
+++ src/config/api.js
@@ -3,8 +3,6 @@
 const API_URL = () =>
   process.env.REACT_APP_API_URL || "https://gentle-forest-86099.herokuapp.com";
 
-// "http://localhost:4000";
-
 export const API_LOGIN = `${API_URL()}/user/login`;
 
 export const API_USER = `${API_URL()}/user/`;
