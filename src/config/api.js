0da6c3adfc0f62fbc94f6df8692d016c5dfb9a4f
Martin <byrlukmartin@gmail.com>
2021-10-11 16:41:18 +0300

    remove env var

--- src/config/api.js
+++ src/config/api.js
@@ -1,11 +1,9 @@
 // API Endpoints
 
-const API_URL = () =>
-  process.env.REACT_APP_API_URL ||
-  "https://liven-backend-3x6qh.ondigitalocean.app";
+const API_URL = "https://liven-backend-3x6qh.ondigitalocean.app";
 
-export const API_LOGIN = `${API_URL()}/user/login`;
+export const API_LOGIN = `${API_URL}/user/login`;
 
-export const API_USER = `${API_URL()}/user/`;
+export const API_USER = `${API_URL}/user/`;
 
-export const API_WORKS = `${API_URL()}/works`;
+export const API_WORKS = `${API_URL}/works`;
