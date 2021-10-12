746db9ad7335621b9b5cd02af5ace6d7309bf9fc
Martin <byrlukmartin@gmail.com>
2021-10-13 00:15:22 +0300

    refactor Login from class to func

--- src/app/views/Login/index.js
+++ src/app/views/Login/index.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { withRouter } from "react-router-dom";
+
+// Material
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
 
 // Actions
 import { login } from "../../../redux/auth/actions";
@@ -10,52 +16,49 @@ import { login } from "../../../redux/auth/actions";
 // Selectors
 import { makeSelectAuthChecked } from "../../../redux/auth/selectors";
 
-// Components
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import Box from "@material-ui/core/Box";
-
 // Helpers
-import { checkEmail } from "../../../utils/helpers";
+import { checkEmail, usePrevious } from "../../../utils/helpers";
 
-// Styles
-import "./styles.scss";
+const useStyles = makeStyles((theme) => ({
+  login: { width: "100%", height: "100vh" },
+  textField: { width: 300, marginTop: theme.spacing(2) },
+  button: { marginTop: theme.spacing(3) },
+}));
 
-class LoginPage extends React.Component {
-  state = {
-    user: {},
-    errors: {},
+const LoginPage = ({ authChecked, onLogin, history, location }) => {
+  const [loginData, setLoginData] = useState({ user: {}, errors: {} });
+  const prevAuthChecked = usePrevious(authChecked);
+  const classes = useStyles();
+
+  const isLogin = location.pathname === "/login";
+  const { errors, user } = loginData;
+
+  const onKeyPressed = (e) => {
+    if (e.keyCode === 13 && isLogin) {
+      handleSubmit();
+      document.removeEventListener("keydown", (e) => onKeyPressed(e));
+    }
   };
 
-  componentDidMount() {
-    document.addEventListener("keydown", (e) => this.onKeyPressed(e));
-  }
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => onKeyPressed(e));
 
-  componentWillUnmount() {
-    document.removeEventListener("keydown", (e) => this.onKeyPressed(e));
-  }
+    return () => {
+      document.removeEventListener("keydown", (e) => onKeyPressed(e));
+    };
+  }, []);
 
-  componentDidUpdate(prevState, prevProps) {
-    const { authChecked, history } = this.props;
-    if (!prevProps.authChecked && authChecked) {
+  useEffect(() => {
+    if (!prevAuthChecked && authChecked) {
       history.push("/management");
     }
-  }
+  });
 
-  onKeyPressed(e) {
-    const { location } = this.props;
-    const isLogin = location.pathname === "/login";
-
-    if (e.keyCode === 13 && isLogin) {
-      this.handleSubmit();
-      document.removeEventListener("keydown", (e) => this.onKeyPressed(e));
-    }
-  }
-
-  validate = () => {
-    const { errors, user } = this.state;
+  const validate = () => {
     const REQUIRED = ["email", "password"];
     const isOutOfBounds = (s) => !s || s.length < 2;
+
+    let currErrors = errors;
 
     REQUIRED.forEach((prop) => {
       if (
@@ -63,74 +66,70 @@ class LoginPage extends React.Component {
         !checkEmail(user[prop]) &&
         !isOutOfBounds(user[prop])
       ) {
-        errors[prop] = "This email is not valid.";
+        currErrors[prop] = "This email is not valid.";
       }
       if (isOutOfBounds(user[prop])) {
-        errors[prop] = "This field is required.";
+        currErrors[prop] = "This field is required.";
       }
     });
 
-    this.setState({ errors });
-    return Object.keys(errors).length === 0;
+    setLoginData({ ...loginData, errors: currErrors });
+    return Object.keys(currErrors).length === 0;
   };
 
-  handleSubmit = () => {
-    const { onLogin } = this.props;
-    const { user } = this.state;
-
-    if (this.validate()) {
-      onLogin(user);
-    }
+  const handleSubmit = () => {
+    if (validate()) onLogin(user);
   };
 
-  handleChange = (prop, data) => {
-    const { errors, user } = this.state;
-    delete errors[prop];
-    user[prop] = data;
+  const handleChange = (prop, data) => {
+    let currUser = user;
+    let currErrors = errors;
 
-    this.setState({ errors, user });
+    delete currErrors[prop];
+    currUser[prop] = data;
+
+    setLoginData({ user: currUser, errors: currErrors });
   };
 
-  render() {
-    const { errors } = this.state;
-
-    return (
-      <div className="login">
-        <Box display="flex" flexDirection="column">
-          <TextField
-            id="email"
-            label="E-mail address"
-            variant="outlined"
-            type="email"
-            error={!!errors?.email}
-            onChange={(e) => this.handleChange("email", e.target.value)}
-            helperText={errors.email}
-            style={{ width: 300, marginTop: 16 }} //TODO: Move inline styles to css
-          />
-          <TextField
-            id="password"
-            label="Password"
-            variant="outlined"
-            type="password"
-            error={!!errors?.password}
-            onChange={(e) => this.handleChange("password", e.target.value)}
-            helperText={errors.password}
-            style={{ width: 300, marginTop: 16 }} //TODO: Move inline styles to css
-          />
-          <Button
-            className="login--form-submit"
-            variant="contained"
-            color="primary"
-            style={{ marginTop: 24 }} //TODO: Move inline styles to css
-            onClick={() => this.handleSubmit()}
-          >
-            Login
-          </Button>
-        </Box>
-      </div>
-    );
-  }
-}
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+      className={classes.login}
+    >
+      <Box display="flex" flexDirection="column">
+        <TextField
+          label="E-mail address"
+          variant="outlined"
+          type="email"
+          className={classes.textField}
+          error={!!errors?.email}
+          onChange={(e) => handleChange("email", e.target.value)}
+          helperText={errors.email}
+        />
+        <TextField
+          label="Password"
+          variant="outlined"
+          type="password"
+          className={classes.textField}
+          error={!!errors?.password}
+          onChange={(e) => handleChange("password", e.target.value)}
+          helperText={errors.password}
+        />
+        <Button
+          className={classes.button}
+          variant="contained"
+          color="primary"
+          onClick={() => handleSubmit()}
+        >
+          Login
+        </Button>
+      </Box>
+    </Box>
+  );
+};
 
 LoginPage.propTypes = {
   authChecked: PropTypes.bool,
