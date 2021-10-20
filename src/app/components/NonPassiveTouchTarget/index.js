8782b3c4ab1933f183d03342e1e5f6af64d881ba
Martin <byrlukmartin@gmail.com>
2021-10-20 22:06:42 +0300

    refactor carousel

---
+++ src/app/components/NonPassiveTouchTarget/index.js
@@ -0,0 +1,53 @@
+/*
+ * For nasty iOS Safari.
+ * Since iOS Safari 11.3+, all touchmove listeners are passive by default.
+ * Yet it doesn't fully support CSS touch-action like Chrome does.
+ * So here is a component to workaround.
+ */
+
+import React from "react";
+import PropTypes from "prop-types";
+
+const OPTIONS = { passive: false };
+
+class NonPassiveTouchTarget extends React.Component {
+  componentDidMount() {
+    this.node.addEventListener("touchmove", this.props.onTouchMove, OPTIONS);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.onTouchMove !== this.props.onTouchMove) {
+      this.node.removeEventListener(
+        "touchmove",
+        prevProps.onTouchMove,
+        OPTIONS
+      );
+      this.node.addEventListener("touchmove", this.props.onTouchMove, OPTIONS);
+    }
+  }
+
+  componentWillUnmount() {
+    this.node.removeEventListener("touchmove", this.props.onTouchMove, OPTIONS);
+  }
+
+  ref = (node) => {
+    this.node = node;
+  };
+
+  render() {
+    const { component: Component, onTouchMove, ...rest } = this.props;
+    return <Component ref={this.ref} onTouchMove={onTouchMove} {...rest} />;
+  }
+}
+
+NonPassiveTouchTarget.propTypes = {
+  component: PropTypes.string,
+  onTouchMove: PropTypes.func,
+};
+
+NonPassiveTouchTarget.defaultProps = {
+  component: "div",
+  onTouchMove() {},
+};
+
+export default NonPassiveTouchTarget;
--- src/app/components/WorkItem/index.js
+++ src/app/components/WorkItem/index.js
@@ -1,162 +1,244 @@
-import React, { useState, Fragment, useEffect } from "react";
+import React, { Fragment, useRef } from "react";
 import PropTypes from "prop-types";
-import { useSelector } from "react-redux";
-import { Carousel } from "react-responsive-carousel";
 import { connect } from "react-redux";
 import { isMobile, isDesktop } from "react-device-detect";
 import { NavLink, withRouter } from "react-router-dom";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-
-// Material
+import TouchCarousel from "react-touch-carousel";
+import touchWithMouseHOC from "react-touch-carousel/lib/touchWithMouseHOC";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
 import IconButton from "@material-ui/core/IconButton";
 import NavigateNextIcon from "@material-ui/icons/NavigateNext";
 import NavigatePrevIcon from "@material-ui/icons/NavigateBefore";
-
-// Selectors
 import { makeSelectWorkById } from "../../../redux/works/selectors";
-
-// Components
+import NonPassiveTouchTarget from "../NonPassiveTouchTarget";
 import Loader from "../Loader";
-
-// Assets
 import placeholder from "../../../assets/placeholder.png";
+import { useEventListener } from "../../../utils/helpers";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
-// Styles
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import "./styles.scss";
+const bdefStyle = {
+  position: "absolute",
+  top: "calc(50% - 0.5rem)",
+  zIndex: 1,
+};
+const windowWidth = window.innerWidth;
 
-const WorkItem = (props) => {
-  const [currentSlide, changeSelectedItem] = useState(0);
-  const { work } = props;
-
-  useEffect(() => {
-    changeSelectedItem(0);
-  }, [work]);
-
-  const worksList = useSelector((state) => state.works.worksList);
-
-  const onChangeSelectedItem = (index) => {
-    if (index !== currentSlide) {
-      changeSelectedItem(index);
-    }
-  };
-
-  const mobileClass = isMobile ? "--mobile" : "--desktop";
-
-  const buttonStyles = {
-    prev: {
-      left: 4,
-      position: "absolute",
-      top: "46%",
-      zIndex: 1,
+const useStyles = makeStyles((theme) => ({
+  workItemWrapper: {
+    height: "inherit",
+  },
+  prevButton: { left: 16, ...bdefStyle },
+  nextButton: { right: 16, ...bdefStyle },
+  workLinkItem: {
+    fontSize: 16,
+    fontWeight: 500,
+    textDecoration: "none",
+    padding: theme.spacing(1),
+    color: theme.palette.grey[900],
+    margin: 0,
+    marginBottom: theme.spacing(1),
+    borderRadius: 4,
+  },
+  workLinksWrapper: {
+    padding: theme.spacing(2),
+    width: "100%",
+    height: "100%",
+    overflowY: "auto",
+  },
+  carouselContainer: {
+    position: "relative",
+    width: "100%",
+    margin: "0 auto",
+    overflow: "hidden",
+    touchAction: "pan-y",
+  },
+  carouselTrack: {
+    display: "flex",
+    height: "100%",
+  },
+  carouselCard: {
+    flex: `0 0 ${isMobile ? windowWidth : windowWidth - 250}px`,
+    overflow: "hidden",
+    userSelect: "none",
+    "&  p": {
+      alignSelf: "center",
     },
-    next: { position: "absolute", top: "47%", right: 4 },
-  };
+  },
+  LazyLoadWrapper: {
+    textAlign: "center",
+  },
+  carouselCardInner: {
+    height: window.innerHeight - (isMobile ? 224 : 168),
+    width: "92%",
+    objectFit: "contain",
+  },
+  carouselPaginationWrapper: {
+    pointerEvents: "none",
+    userSelect: "none",
+  },
+  carouselPagination: {
+    padding: theme.spacing(3),
+    margin: 0,
+  },
+}));
 
-  if (isMobile && !work?.id) {
+const WorkItem = ({ work, workLinks }) => {
+  const classes = useStyles({ isDesktop });
+  const cardSize = isMobile ? windowWidth : windowWidth - 250;
+  const cardPadCount = 1;
+  const photosCount =
+    work !== undefined && Object.values(work?.photos)?.length + 1;
+  const carousel = useRef({
+    next: () => null,
+    prev: () => null,
+  });
+
+  useEventListener("keydown", (e) => {
+    if (e.code === "ArrowRight") {
+      carousel && carousel.current.next();
+    }
+    if (e.code === "ArrowLeft") {
+      carousel && carousel.current.prev();
+    }
+  });
+
+  const CarouselContainer = (props) => {
+    const { cursor, carouselState, ...rest } = props;
+
+    let current = -Math.round(cursor) % photosCount;
+    while (current < 0) {
+      current += photosCount;
+    }
+    const translateX = (cursor - cardPadCount) * cardSize;
+
     return (
-      <div className="work-items">
-        {Object.values(worksList).map((navItem) => {
-          const { name, id } = navItem;
-
-          return (
-            <NavLink
-              key={id}
-              className={`navbar__sub-item navbar__item`}
-              to={`works?${id}`}
-              title={name}
-            >
-              {name}
-            </NavLink>
-          );
-        })}
-      </div>
-    );
-  }
-
-  if (!!work?.id) {
-    return (
-      <Fragment>
-        <Carousel
-          autoPlay={false}
-          className={`carousel${mobileClass}`}
-          showIndicators={false}
-          showStatus={false}
-          showThumbs={false}
-          showArrows={!isMobile}
-          selectedItem={currentSlide}
-          useKeyboardArrows
-          swipeable
-          onChange={onChangeSelectedItem}
-          renderArrowPrev={(onClickHandler, hasPrev) =>
-            hasPrev && (
+      <Box
+        display="flex"
+        flexDirection="column"
+        className={classes.workItemWrapper}
+      >
+        <NonPassiveTouchTarget
+          className={classes.carouselContainer}
+          carouselstate={carouselState}
+        >
+          {isDesktop && (
+            <Fragment>
               <IconButton
-                style={buttonStyles.prev} //TODO: Move inline styles to css
+                className={classes.prevButton}
                 aria-label="prev"
-                onClick={onClickHandler}
+                onClick={() => carousel && carousel.current.prev()}
               >
                 <NavigatePrevIcon />
               </IconButton>
-            )
-          }
-          renderArrowNext={(onClickHandler, hasNext) =>
-            hasNext && (
+
               <IconButton
-                style={buttonStyles.next} //TODO: Move inline styles to css
+                className={classes.nextButton}
                 aria-label="next"
-                onClick={onClickHandler}
+                onClick={() => carousel && carousel.current.next()}
               >
                 <NavigateNextIcon />
               </IconButton>
-            )
-          }
+            </Fragment>
+          )}
+          <NonPassiveTouchTarget
+            className={classes.carouselTrack}
+            style={{ transform: `translate3d(${translateX}px, 0, 0)` }}
+            {...rest}
+          />
+        </NonPassiveTouchTarget>
+        <Box
+          display="flex"
+          justifyContent="flex-end"
+          className={classes.carouselPaginationWrapper}
         >
-          {Object.values(work?.photos)?.map((photo, index) => {
-            return (
-              <div key={index} className="slide-item">
+          <p className={classes.carouselPagination}>
+            {current + 1} / {photosCount} {work.name}
+          </p>
+        </Box>
+      </Box>
+    );
+  };
+
+  if (!!work?.id) {
+    return (
+      <TouchCarousel
+        component={touchWithMouseHOC(CarouselContainer)}
+        cardSize={cardSize}
+        loop
+        cardCount={photosCount}
+        cardPadCount={1}
+        ref={carousel}
+        renderCard={(index, modIndex) => {
+          const item = work?.photos[modIndex];
+
+          return (
+            <Box
+              key={index}
+              display="flex"
+              justifyContent="center"
+              alignitems="center"
+              className={classes.carouselCard}
+            >
+              {photosCount === index + 1 ? (
+                <p>{work?.description}</p>
+              ) : (
                 <LazyLoadImage
+                  effect="blur"
+                  placeholderSrc={placeholder}
+                  className={classes.carouselCardInner}
+                  wrapperClassName={classes.LazyLoadWrapper}
+                  src={item?.img || ""}
+                  alt="liven_img"
                   onError={(e) => {
                     e.target.onerror = null;
-                    e.target.src = placeholder;
                   }}
-                  className={`carousel-image${mobileClass}`}
-                  src={photo?.img || ""}
-                  alt={"liven_img"}
                 />
-              </div>
+              )}
+            </Box>
+          );
+        }}
+      />
+    );
+  }
+
+  if (isMobile && !work?.id) {
+    return (
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="flex-start"
+        className={classes.workLinksWrapper}
+      >
+        {workLinks !== undefined &&
+          workLinks.map((navItem) => {
+            const { name, workId } = navItem;
+
+            return (
+              <NavLink
+                key={workId}
+                className={classes.workLinkItem}
+                to={`works?${workId}`}
+                title={name}
+              >
+                {name}
+              </NavLink>
             );
           })}
-          <div className={`slide-item${mobileClass} description--wrapper`}>
-            <p className={`description--${isDesktop ? "desktop" : "mobile"}`}>
-              {work.description}
-            </p>
-          </div>
-        </Carousel>
-        <p
-          style={{
-            textAlign: "right",
-            paddingRight: isMobile ? 24 : 42,
-            userSelect: "none",
-          }}
-        >
-          {currentSlide + 1} / {Object.values(work?.photos).length + 1}{" "}
-          {work.name}
-        </p>
-      </Fragment>
+      </Box>
     );
   }
 
   return <Loader />;
 };
 
-// Props
-
 WorkItem.propTypes = {
   work: PropTypes.object,
   workId: PropTypes.string,
-  location: PropTypes.object,
-  onToggleTheme: PropTypes.func,
-  carouselTheme: PropTypes.string,
+  workLinks: PropTypes.array,
+  cursor: PropTypes.any,
+  carouselState: PropTypes.object,
 };
 
 const mapStateToProps = () => {
